06d894f8c7bd621986e1b523ed3bc33c8b8e4de5
dirana21 <levchenko.98989878@gmail.com>
2025-03-26 13:14:12 +0300

    добавил 4 лабораторную

---
+++ LAB_4(Done)/Task1/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Task1
+{
+    using System.Text.RegularExpressions;
+    internal class Program
+    {
+        static void FirstTask()
+        {
+            /*
+             1. Написати регулярний вираз, який визначає чи є заданий рядок рядком "abcdefghijklmnopqrstuv18340" чи ні.
+                – правильний вираз: abcdefghijklmnopqrstuv18340.
+                – невірний вираз:  abcdefghijklmnoasdfasdpqrstuv18340.
+            */
+            string pattern = @"^abcdefghijklmnopqrstuv18340$";
+            string[] testStrings = {
+                "abcdefghijklmnopqrstuv18340",
+                "abcdefghijklmnoasdfasdpqrstuv18340",
+                "abcdefghijklmnopqrstuv1834",
+                "abcdefghijklmnopqrstuv18340xyz"
+            };
+            foreach (string test in testStrings)
+            {
+                Console.WriteLine($"Строка \"{test}\" " + (Regex.IsMatch(test, pattern) ? "Match" : "Not match") + " shablon.");
+            }
+        }
+        static void SecondTask()
+        {
+            /*
+             2. Написати регулярний вираз, який визначає чи є заданий рядок GUID з дужками або без дужок. GUID – це рядок, що складається з 8, 4, 4, 4, 12 шістнадцяткових цифр розділених тире.
+                – правильний вираз: e02fd0e4-00fd-090A-ca30-0d00a0038ba0;
+                – приклад неправильних виразів:e02fd0e400fd090Aca300d00a0038ba0.
+            */
+            string pattern = @"^\{?[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}\}?$";
+            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            string[] testCases = {
+                "e02fd0e4-00fd-090A-ca30-0d00a0038ba0",
+                "{e02fd0e4-00fd-090A-ca30-0d00a0038ba0}",
+                "e02fd0e400fd090Aca300d00a0038ba0",
+                "e02fd0e4-00fd-090A-ca30-0d00a0038ba0-",
+                "{e02fd0e4-00fd-090A-ca30-0d00a0038ba0" 
+            };
+
+            foreach (var testCase in testCases)
+            {
+                Console.WriteLine($"{testCase} -> {regex.IsMatch(testCase)}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            FirstTask();
+            SecondTask();
+        }
+    }
+}
+
---
+++ LAB_4(Done)/Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            Console.WriteLine("\t--Несортированый текст--");
+            foreach (string i in lines)
+            {
+                Console.WriteLine($"{i}");
+            }
+            string pattern = @"[\s.,!?;:]+";
+            Console.WriteLine("\t--Сортированый текст--");
+            foreach (string value in lines)
+            {
+                string[] words = Regex.Split(value, pattern);
+                for (int i = 0; i < words.Length; i++)
+                {
+                    for (int j = 0; j < words[i].Length - 1; j++)
+                    {
+                        char firstChar = char.ToLower(words[i][0]);
+                        char secondChar = char.ToLower(words[i][j + 1]);
+                        if (firstChar == secondChar)
+                        {
+                            Console.WriteLine($"В этом слове мы нашли еще одну одинаковую букву: {words[i]}");
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
